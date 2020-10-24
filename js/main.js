4cdea4025d78422020934ce8edad1220df742194
Alan dos Anjos Farias <farias_alan@outlook.com>
2020-10-24 15:51:38 -0300

    add js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var nome = "Alan dos Anjos";
+//alert("Bem Vindo " + nome);
+var lista = ['pera', 'maca', 'banana'];
+console.log(lista);
+lista.push('uva');
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join("|"));
+
+//dicionario - parecido com objeto jason
+
+var fruta = {nome: "maca", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
